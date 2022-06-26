750885ff774ff354e63d08578aed07f55fb5bda2
GoGangH <rhrkdgus1020@naver.com>
2022-06-27 02:22:10 +0900

    "초안 find 부분을 수정해야 할것"

---
+++ Review_crowling.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+def crowling():
+    get_search = input('노래 제목을 입력하시오: ')
+
+    url = 'http://izm.co.kr/searchLists.asp?search_tp=8&keywordid=&keyword=' + urllib.parse.quote(get_search)
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
+    response = requests.get(url,headers=headers)
+
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+    else : 
+        print(response.status_code)
+        return
+
+    title = soup.find_all(class_='contents4')
+    
+    for i in title:
+        print(i.attrs['href'])
+        print()
+
+def main():
+    crowling()
+
+if __name__ == "__main__":
+	main()
