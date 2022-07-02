a8985996212b963697778531f5308006c8d60b66
GoGangH <rhrkdgus1020@naver.com>
2022-07-02 17:22:43 +0900

    "페이지 정보 크롤링 완성"

--- crowling_selenium.py
+++ crowling_selenium.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import os
 import time
 
+from sqlalchemy import false
+
 #step2.검색할 키워드 입력
 query = input('검색할 키워드를 입력하세요: ')
 query2 = input('가수명을 입력하세요: ')
+query = query.replace(" ", "")
+query2 = query2.replace(" ", "")
 
 #step3.크롬드라이버로 원하는 url로 접속
 chromeURL = os.getcwd()+'/chromedriver'
@@ -14,14 +19,36 @@ print(chromeURL)
 url = 'http://izm.co.kr/searchLists.asp?search_tp=8&keywordid=&keyword='+query
 driver = webdriver.Chrome(chromeURL)
 driver.get(url)
-time.sleep(3)
+time.sleep(2)
 
-# continue_link = driver.find_element_by_partial_link_text(query2)
-# continue_link.click()
+''' 제목과 가수에 맞는 설명 페이지 이동 '''
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-titles = soup.select('#summary1 > li > table > tbody > tr')
+music_list = driver.find_elements(By.CSS_SELECTOR, '#summary1 > li > table > tbody > tr')
 
-print(titles)
+chk_M = False
+
+# 페이지 이동
+for i in range(1, len(music_list)+1):
+    Music_name = driver.find_element(By.CSS_SELECTOR, f"#summary1 > li.contents4 > table > tbody > tr:nth-child({i}) > td > table > tbody > tr > td:nth-child(1)").text.replace("\n","")
+    Music_singer = driver.find_element(By.CSS_SELECTOR, f"#summary1 > li.contents4 > table > tbody > tr:nth-child({i}) > td > table > tbody > tr > td:nth-child(3)").text.replace("\n","")
+    if Music_name == query and Music_singer == query2 :
+        print("select : " , Music_name, " ", Music_singer)
+        driver.find_element(By.XPATH, f"/html/body/ul/li[3]/ul/li[1]/div/ul/li[2]/table/tbody/tr[{i}]/td/table/tbody/tr/td[1]/a").click()
+        chk_M=True
+
+if not chk_M :
+    print("제목과 가수를 다시 확인해주세요!")
+
+time.sleep(1)
+
+'''페이지 리뷰 설명 가져오기'''
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+review_list = driver.find_element(By.CSS_SELECTOR, f"#summary1 > li.contents3 > article").text.replace("\n", " ")
+
+print(review_list)
